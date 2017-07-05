fa36a52553ec85630ccd469972c735857f87e13a
Alyssa Moody and Daniela Trulls <alyssa+daniela@>
2017-07-05 10:43:52 -0700

    Add bold to strong drinks. Decrease pints on button click, turn background red when less than 10 left.

--- app/app.component.ts
+++ app/app.component.ts
@@ -7,14 +7,11 @@ import { Component } from '@angular/core';
     <h1>Taproom</h1>
     <h3>Currently on tap:</h3>
     <ul>
-      <li [class]="priceColor(currentKeg)" (click)="isEmpty(currentKeg)" *ngFor="let currentKeg of kegs">{{currentKeg.brand}}, {{currentKeg.name}}, {{currentKeg.alcoholContent}}, $ {{currentKeg.price}} <button (click)="editKeg(currentKeg)">Edit!</button></li>
+      <li [class]="isAlmostEmpty(currentKeg)" (click)="isAlmostEmpty(currentKeg)" *ngFor="let currentKeg of kegs"><span [class]="strong(currentKeg)">{{currentKeg.brand}}, {{currentKeg.name}},  {{currentKeg.alcoholContent}}%, <span [class]="priceColor(currentKeg)">$ {{currentKeg.price}} </span>, {{currentKeg.pints}}</span> <button (click)="editKeg(currentKeg)">Edit!</button> <button (click)="sellPint(currentKeg)">Sold a Pint!</button></li>
     </ul>
     <hr>
     <div>
-      <h3>{{selectedKeg.name}}</h3>
-      <p>Keg Complete? {{selectedKeg.empty}}</p>
-      <h3>Edit Keg</h3>
-      <label>Enter Keg's New Price per pint:</label>
+      <label><h3>Edit {{selectedKeg.name}}'s Price/pint:</h3></label>
       <input [(ngModel)]="selectedKeg.price">
     </div>
     <div>
@@ -39,15 +36,15 @@ import { Component } from '@angular/core';
 
 export class AppComponent {
   kegs: Keg[] = [
-    new Keg('Chocolate Hazelnut Porter', 'Heretic', 6, '7.0%'),
-    new Keg('Herbs of a Feather', 'Coalition', 6, '5.5%'),
-    new Keg('Spruce Saison', 'Propolis', 8, '7.5%'),
-    new Keg('Brett IPA', 'pFriem', 6, '6.6%'),
-    new Keg('Rocketman Red', 'Rocketman Red', 5, '5.5%'),
-    new Keg('Porter', 'Anchor', 5, '5.6%'),
-    new Keg('Dry-Hopped Barleywine', 'Sierra Nevada/Avery', 7, '9.4%'),
-    new Keg('Hoppy A-Hefe', 'StormBreaker', 6, '5.0%'),
-    new Keg('Swami’s', 'Pizza Port', 6, '6.8%')  ];
+    new Keg('Chocolate Hazelnut Porter', 'Heretic', 6, 7.0),
+    new Keg('Herbs of a Feather', 'Coalition', 6, 5.5),
+    new Keg('Spruce Saison', 'Propolis', 8, 7.5),
+    new Keg('Brett IPA', 'pFriem', 6, 6.6),
+    new Keg('Rocketman Red', 'Rocketman Red', 5, 5.5),
+    new Keg('Porter', 'Anchor', 5, 5.6),
+    new Keg('Dry-Hopped Barleywine', 'Sierra Nevada/Avery', 7, 9.4),
+    new Keg('Hoppy A-Hefe', 'StormBreaker', 6, 5.0),
+    new Keg('Swami’s IPA', 'Pizza Port', 6, 6.8)  ];
   selectedKeg: Keg = this.kegs[0];
 
   priceColor(currentKeg){
@@ -60,20 +57,28 @@ export class AppComponent {
     }
   }
 
+  strong(currentKeg){
+    if(currentKeg.alcoholContent <= 6.5){
+      return "strong";
+    }
+  }
+
   editKeg(clickedKeg) {
     this.selectedKeg = clickedKeg;
   }
 
-  addKeg(name: string, brand: string, price: number, alcoholContent: string) {
+  sellPint(clickedKeg: Keg){
+    clickedKeg.pints -= 1;
+  }
+
+  addKeg(name: string, brand: string, price: number, alcoholContent: number) {
     var newKeg = new Keg(name, brand, price, alcoholContent);
     this.kegs.push(newKeg);
   }
 
-  isEmpty(clickedKeg: Keg) {
-    if(clickedKeg.empty === true) {
-      alert("This keg is done!");
-    } else {
-      alert("This keg is not done. Better get to work!");
+  isAlmostEmpty(clickedKeg: Keg) {
+    if(clickedKeg.pints <= 10) {
+      return "bg-danger";
     }
   }
 }
@@ -81,5 +86,5 @@ export class AppComponent {
 export class Keg {
   public empty: boolean = false;
   public pints: number = 124;
-  constructor(public name: string, public brand: string, public price: number, public alcoholContent: string) {   }
+  constructor(public name: string, public brand: string, public price: number, public alcoholContent: number) {   }
 }
